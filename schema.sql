efabba58eff9b23de5ac78b891c4204cf5a172f6
Michael Cassaro <mdcassaro@hotmail.com>
2020-04-21 18:44:54 -0500

    tables added

---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_tracker_DB;
+CREATE database employee_tracker_DB;
+
+USE employee_tracker_DB;
+
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(30,2),
+    dapartment_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+)
